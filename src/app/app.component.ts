95e8e786301a4d8342702f3afd1fbd1a92cd88ed
Woodmaninator <woodmaninator@gmail.com>
2024-01-21 20:50:11 +0100

    initial commit with empty project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wp-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'wikipedle';
+}
