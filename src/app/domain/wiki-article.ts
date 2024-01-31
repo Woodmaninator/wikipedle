facbad44a61e7a7e9466690be5dd7c6563852be7
Woodmaninator <woodmaninator@gmail.com>
2024-01-31 02:38:01 +0100

    improved UI for mobile and added background images

--- src/app/domain/wiki-article.ts
+++ src/app/domain/wiki-article.ts
@@ -1,8 +1,9 @@
-export class WikiArticle{
-    constructor(
-        public id: number,
-        public title: string,
-        public averageViews: number,
-        public description = ""
-    ) {}
+export class WikiArticle {
+  constructor(
+    public id: number,
+    public title: string,
+    public averageViews: number,
+    public description = '',
+    public imageUrl = ''
+  ) {}
 }
